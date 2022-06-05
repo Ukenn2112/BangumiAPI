3296d5ce7648bc99599f2337f8a50bd6d4b98dde
Ukenn <duola2002@gmail.com>
2022-06-05 20:43:01 +0900

    评论接口完成

--- api.py
+++ api.py
@@ -1,62 +1,67 @@
-import json
 import requests
-# 載入Flask套件
 from flask import Flask
-# 載入Flask RestFul 套件
+from lxml import etree
 from flask_restful import Api, Resource
+from xml.etree.ElementTree import tostring
 
 # 創建Flask app物件
 app = Flask(__name__)
 # 創建Flask api物件
 api = Api(app)
 
+session = requests.session()
+
 # 創建Products 物件並繼承Resource物件
 class Products(Resource):
     def get(self, episode_id):
         # 取得資料
-        user_agent = (
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-            'Chrome/99.0.4844.51 Safari/537.36'
-        )
-        r = requests.get(f'https://api.bgm.tv/v0/episodes/{episode_id}', headers={'User-Agent': user_agent})
-        # 回傳資料
-        data = json.loads(r.text)
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46",
+        }
+        r = session.get(f"https://bgm.tv/ep/{episode_id}", headers=headers)
+        html = etree.HTML(r.content)
+        error = html.xpath('//*[@id="colunmNotice"]/div/p[1]')
+        if error:
+            return {"error": 'Episode not found'}, 404
+        # 取得資料
+        subject_id = html.xpath('//*[@id="headerSubject"]/h1/a/@href')[0].lstrip('/subject/')
+        comment_list = html.xpath('//*[@id="comment_list"]/div/@name')
+        comment_data_list = []
+        for i in comment_list:
+            reply_comment_list = html.xpath(f'//div[@name="{i}"]/div[2]/div/div[@class="topic_sub_reply"]/div/@name')
+            reply_list = []
+            for r in reply_comment_list:
+                reply_data = {}
+                reply_data['floor'] = r
+                reply_data['floor_time'] = html.xpath(f'//div[@name="{r}"]/div[1]/small/text()')[0].lstrip(' - ').rstrip(" ")
+                reply_data['from_name'] = html.xpath(f'//div[@name="{r}"]/div[2]/strong/a/text()')[0]
+                reply_data['from_link'] = "https://bgm.tv" + html.xpath(f'//div[@name="{r}"]/div[2]/strong/a/@href')[0]
+                reply_data['from_avatar'] = "https:" + html.xpath(f'//div[@name="{r}"]/a/span/@style')[0].lstrip("background-image:url('").rstrip("')")
+                reply_data['comment'] = tostring(html.xpath(f'//div[@name="{r}"]/div[2]/div[@class="cmt_sub_content"]')[0], encoding="utf-8").decode("utf-8")
+                reply_list.append(reply_data)
+
+            comment_data = {}
+            comment_data["floor"] = i
+            comment_data["floor_time"] = html.xpath(f'//div[@name="{i}"]/div[1]/small/text()')[0].lstrip(' - ')
+            comment_data["from_name"] = html.xpath(f'//div[@name="{i}"]/div[2]/strong/a/text()')[0]
+            comment_data["from_link"] = "https://bgm.tv" + html.xpath(f'//div[@name="{i}"]/div[2]/strong/a/@href')[0]
+            comment_data["from_avatar"] = "https:" + html.xpath(f'//div[@name="{i}"]/a/span/@style')[0].lstrip("background-image:url('").rstrip("')")
+            comment_data["comment"] = tostring(html.xpath(f'//div[@name="{i}"]/div[2]/div/div[@class="message clearit"]')[0], encoding="utf-8").decode("utf-8")
+            comment_data["reply"] = reply_list
+            comment_data_list.append(comment_data)
+
         output = {
-            "ep_id": data['id'],
-            "subject_id": data['subject_id'],
-            "comment": data['comment'],
-            "data": [
-                {   
-                    "floor": "1",
-                    "from_name": "河豚酱fuguchan",
-                    "from_link": "https://bgm.tv/user/fuguchan1145141",
-                    "from_avatar": "https://lain.bgm.tv/pic/user/l/000/62/03/620315.jpg?r=1628535454",
-                    "comment": "长颈龙分镜，复刻明日酱ep8？",
-                    "reply": [
-                        {
-                            "floor": "1-1",
-                            "from_name": "河豚酱fuguchan",
-                            "from_link": "https://bgm.tv/user/fuguchan1145141",
-                            "from_avatar": "https://lain.bgm.tv/pic/user/l/000/62/03/620315.jpg?r=1628535454",
-                            "comment": "长颈龙分镜，复刻明日酱ep8？",
-                            "reply": [],
-                        },
-                        {
-                            "floor": "1-2",
-                            "from_name": "河豚酱fuguchan",
-                            "from_link": "https://bgm.tv/user/fuguchan1145141",
-                            "from_avatar": "https://lain.bgm.tv/pic/user/l/000/62/03/620315.jpg?r=1628535454",
-                            "comment": "长颈龙分镜，复刻明日酱ep8？",
-                            "reply": [],
-                        },
-                    ],
-                },
-            ]
+            "ep_id": int(episode_id),
+            "subject_id": int(subject_id),
+            "comment": len(comment_list),
+            "data": comment_data_list,
         }
         return output, 200
+
 
 # 建立API路由products，並將該路由導向Products物件
 api.add_resource(Products, '/episodes/comments/<int:episode_id>')
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run('0.0.0.0', port=8080, debug=False)
